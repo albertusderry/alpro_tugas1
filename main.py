9982a9f02d6112ca31338d4f1a3079138328fbd5
albertusderry <32255769+albertusderry@users.noreply.github.com>
2017-09-28 22:39:54 +0700

    Derry Pramono Adi - 04216086

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+
+nama = "Derry Pramono Adi"
+nim = "04216086"
+umur = int(19)
+umurlima = int(umur+5)
+
+print ("Nama:", nama)
+print ("NIM :", nim)
+print ("Usia:", umur, "tahun")
+print ("Usia Derry 5 tahun lagi :", umurlima, "tahun")
